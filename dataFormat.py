34c977a2a99e3114a130fc035548a82dbc5c1019
AriSpyrou <p15132@students.cs.unipi.gr>
2018-01-09 11:27:28 +0200

    added data files

--- dataFormat.py
+++ dataFormat.py
@@ -1,12 +1,14 @@
 import csv
+import numpy as np
 
-
+'''
 with open('.\data\dataN.csv', 'rb') as src, open('.\data\dataN2.csv', 'wb') as res:
     csrc = csv.reader(src, delimiter=',')
     cres = csv.writer(res, delimiter=',')
     for row in csrc:
         del row[0:3]
         cres.writerow(row)
+'''
 
 '''
 with open('.\.ignore\ml-100k\u.data', 'rb') as src, open('data.csv', 'wb') as res:
@@ -16,3 +18,42 @@ with open('.\.ignore\ml-100k\u.data', 'rb') as src, open('data.csv', 'wb') as re
         newrow = row[:3]
         cres.writerow(newrow)
 '''
+
+'''
+data = np.genfromtxt('data/.ignore/dataN.csv', delimiter=',')
+res = [[0 for i in range(1682)] for j in range(943)]
+for row in data:
+    res[int(row[0])-1][int(row[1])-1] = 1
+res = np.array(res).astype(int)
+np.savetxt("data\data1.csv", res, delimiter=",", fmt='%d')
+'''
+
+'''
+data = np.genfromtxt('data/.ignore/dataN.csv', delimiter=',')
+res = [[0 for i in range(18)] for j in range(943)]
+for row in data:
+    i = 0
+    for col in row[3:]:
+        if col == 1:
+            res[int(row[0])-1][i] += 1
+        i += 1
+res = np.array(res).astype(int)
+np.savetxt("data\data2.csv", res, delimiter=",", fmt='%d')
+'''
+
+'''
+data = np.genfromtxt('data/data2.csv', delimiter=',')
+matrix = []
+for row in data:
+    cnt = 0
+    my_row = []
+    for col in row:
+        cnt += col
+    for col in row:
+        col = col/cnt
+        my_row.append(col)
+    matrix.append(my_row)
+matrix = np.array(matrix)
+np.savetxt("data\data2N.csv", matrix, delimiter=",", fmt='%.5f')
+'''
+
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import numpy as np
 import math
-import csv
 
 data = np.genfromtxt('data/dataN.csv', delimiter=',')
+np.random.shuffle(data)
 N = data.shape[0]  # number of vectors to cluster
 mC = []  # list of vectors that represent the whole cluster
 result = []  # list of different number of cluster
 
 dmin = 0
 dmax = math.sqrt(10)
-Theta_min = 0.9  # dmin + 0.25*(dmax - dmin)
-Theta_max = 1.9  # dmin + 0.75*(dmax - dmin)
-Theta_step = 2
+Theta_min = 0.99  # dmin + 0.25*(dmax - dmin) used initially to approxiamate
+Theta_max = 1.74  # dmin + 0.75*(dmax - dmin) used initially to approxiamate
+Theta_step = 0.0075
 Theta_Range = np.arange(Theta_min, Theta_max, Theta_step)
 
 for Theta in Theta_Range:
@@ -23,17 +23,15 @@ for Theta in Theta_Range:
     # cluster the rest of the vectors
     for i in range(1, N):
         distance_min = np.linalg.norm(mC[0]-data[i])  # set as min the distance from the first cluster
-        index_min = 1  # number of the selected cluster
         for k in range(1, m):
             distance = np.linalg.norm(mC[k]-data[i])  # distance between all clusters and vector
             if distance_min > distance:
                 distance_min = distance  # keep the min
-                index_min = k  # number of the selected cluster
 
         if distance_min > Theta:  # if vectors min distance is less than the theta create a new cluster
             m = m + 1
             mC.append(data[i])  # always set as representative of a new cluster the vector that triggered its creation
     result.append(m)  # add number of clusters for the current repetition
 output = np.column_stack((Theta_Range, result))
-np.savetxt("data\dump\dump2.csv", output, delimiter=",")
-raw_input('press any key')
+np.savetxt("data\dump\dump1.csv", output, delimiter=",", fmt='%.4f')
+#  raw_input('press any key')
