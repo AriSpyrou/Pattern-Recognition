d0f7a751994969f04e55a5185747f5846b9756c3
AriSpyrou <p15132@students.cs.unipi.gr>
2018-01-20 21:21:48 +0200

    updated hc

--- Hierarchical_Clustering.py
+++ Hierarchical_Clustering.py
@@ -3,15 +3,17 @@ from scipy.cluster.hierarchy import dendrogram, linkage
 import numpy as np
 
 data = np.genfromtxt('data/data1N.csv', delimiter=',')
-Z = linkage(data, method='single', metric='euclidean') #single for all points i in cluster u and j in cluster v min distance
+Z = linkage(data, method='median', metric='euclidean')
 
-plt.figure(figsize=(25, 10))
+plt.figure(figsize=(19, 10))
 plt.title('Hierarchical Clustering Dendrogram')
 plt.xlabel('sample index')
 plt.ylabel('distance')
 dendrogram(
     Z,
-    leaf_rotation=90.,  # rotates the x axis labels
-    leaf_font_size=10.,  # font size for the x axis labels
+    leaf_rotation=90,  # rotates the x axis labels
+    leaf_font_size=10,  # font size for the x axis labels
+    truncate_mode='lastp',
+    p=100
 )
 plt.show()
