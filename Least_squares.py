91e4fd84f626393d32c790482fbe38d8d528542d
AriSpyrou <p15132@students.cs.unipi.gr>
2018-01-26 19:54:17 +0200

    least_sq finilized

--- Least_squares.py
+++ Least_squares.py
@@ -1,48 +1,41 @@
 import numpy as np
 import scipy.optimize
 
-movie_id = 50
-fold_number = 2
-offset = 0
+movie_id = 50  # Movie identifier bare in mind that if movie_id>841 then offset should be set to 1
+fold_number = 2  # Which fold we're using to test; should be the same we used to train
+offset = 0  # See movie_id
+
+# Initialize matrices
 data = np.genfromtxt('data/5fold/u'+str(fold_number)+'.base', delimiter=',').astype(int)
 X = []
 Y = []
 x0 = np.ndarray
 
-for i in range(941, 1682):
-    data = np.delete(data, 941, axis=1)
+for i in range(841):  # Delete data that don't fit because dimensions have to be as many as the data points
+    data = np.delete(data, 841*((offset-1)**2), axis=1)
 
 
-def get_error(params, X, Y):
+def get_error(params, X, Y):  # Error function
     return Y - np.dot(X, params)
 
 
+# Create X, Y matrix
 for i in range(data.shape[0]-1):
     X.append(data[i])
     X[i] = np.append(X[i], 1)
-    if X[i][movie_id-offset] == 1:
+    if X[i][movie_id-offset*841] == 1:
         Y.append(1)
     else:
         Y.append(-1)
 
+# Free memory
+del data
+
+# Convert all matrices to ndarrays for uniform treatment
 X = np.array(X).astype(int)
 Y = np.array(Y).astype(int)
 x0 = np.ones(X.shape[1]).astype(float)
 
+# Optimize for errors and print the weight matrix in a file
 x0 = scipy.optimize.leastsq(get_error, x0, args=(X, Y))
-np.savetxt("data\w"+str(movie_id)+".csv", x0[0], delimiter=",", fmt='%.18f')
-
-'''
-precision = 0.0
-for i in range(x0[0].shape[0]-1):
-    predicted = 0
-    for j in range(0, 941):
-        predicted += x0[0][j]*X[i][j]
-    if (predicted*Y[i]) > 0:
-        precision += 1
-        print('Correct')
-    else:
-        print('Incorrect')
-
-print(str((precision/x0[0].shape[0])*100)+'%')
-'''
+np.savetxt("data\w"+'f'+str(fold_number)+'m'+str(movie_id)+".csv", x0[0], delimiter=",", fmt='%.18f')
--- Least_squares_test.py
+++ Least_squares_test.py
@@ -1,32 +1,40 @@
 import numpy as np
-import scipy.optimize
 
-movie_id = 50
-fold_number = 2
-offset = 0
+movie_id = 50  # Movie identifier bare in mind that if movie_id>841 then offset should be set to 1
+fold_number = 2  # Which fold we're using to test; should be the same we used to train
+offset = 0  # See movie_id
+
+# Initialize matrices
 data = np.genfromtxt('data/5fold/u'+str(fold_number)+'.test', delimiter=',').astype(int)
-x0 = np.genfromtxt('data/w'+str(movie_id)+'.csv', delimiter=',').astype(np.float64)
+x0 = np.genfromtxt('data\w'+'f'+str(fold_number)+'m'+str(movie_id)+'.csv', delimiter=',').astype(np.float64)
 X = []
 Y = []
 
-for i in range(941, 1682):
-    data = np.delete(data, 941, axis=1)
+for i in range(841):  # Delete data that don't fit because dimensions have to be as many as the data points
+    data = np.delete(data, 841*((offset-1)**2), axis=1)
 
+# Create X, Y matrix
 for i in range(data.shape[0]-1):
     X.append(data[i])
     X[i] = np.append(X[i], 1)
-    if X[i][movie_id-offset] == 1:
+    if X[i][movie_id-offset*841] == 1:
         Y.append(1)
     else:
         Y.append(-1)
 
+# Free memory
+del data
+
+# Convert all matrices to ndarrays for uniform treatment
 X = np.array(X).astype(int)
 Y = np.array(Y).astype(int)
 
+# Prints whether or not the prediction is correct as well as
+# the percentage of the successfully classified data points
 precision = 0.0
-for i in range(x0.shape[0]-1):
+for i in range(X.shape[0]-1):
     predicted = 0
-    for j in range(0, 941):
+    for j in range(X.shape[1]-1):
         predicted += x0[j]*X[i][j]
     if (predicted*Y[i]) > 0:
         precision += 1
@@ -36,10 +44,12 @@ for i in range(x0.shape[0]-1):
 
 print(str((precision/x0.shape[0])*100)+'%')
 
+# Takes input from the keyboard and predicts whether or not
+# the user with matching user_id has watched the movie (movie_id)
 while True:
     user_id = input('Type a user ID\n')
     decision = 0
-    for i in range(0, 941):
+    for i in range(X.shape[1]-1):
         decision += x0[i]*X[user_id][i]
     if decision > 0:
         print('User '+str(user_id)+' has seen the movie.\n')
